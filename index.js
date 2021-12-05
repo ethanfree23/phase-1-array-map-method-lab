1c667aee10f02ec2dd58bc3683f0f1869b5cfb38
Ethan Freeman <ethanfree23@gmail.com>
2021-12-04 23:18:04 -0600

    commit tree 1dc249ad7f7a4cfa28f13f96100b6ad6f136e822

--- index.js
+++ index.js
@@ -30,11 +30,77 @@ const tutorials = [
 //     element.split(' ').map(word => word.charAt(0).toUpperCase() + word.substring(1)).join(' ')
 //   )}
 
-  const titleCased = () => {
-    return tutorials.map(function(splitted) {
-      return splitted.split(' ').map(function(capitalizeFirstLetter) {
-        return capitalizeFirstLetter.charAt(0).toUpperCase() + capitalizeFirstLetter.slice(1)
-      }).join(' ')
-    })
-  }
+  // const titleCased = () => {
+  //   return tutorials.map(function(splitted) {
+  //     return splitted.split(' ').map(function(capitalizeFirstLetter) {
+  //       return capitalizeFirstLetter.charAt(0).toUpperCase() + capitalizeFirstLetter.slice(1)
+  //     }).join(' ')
+  //   })
+  // }
   
+
+const titleCased = () => {
+  return tutorials.map(function(splitted) {
+    return splitted.split(' ').map(function(capitalizeFirstLetter){
+      return capitalizeFirstLetter.charAt(0).toUpperCase() + capitalizeFirstLetter.slice(1)
+    }).join(' ')
+  })
+}
+//from my understanding, we are referencing tutorials, then splitting each word, the with the split words, we capitalize each letter by specifiying the charAt(0).toUpperCase() then inserting it back in by .slice(1), then joining the words back together. I do not think i can ever pass this lab.
+
+  /*
+  NOTES:
+
+  .map() transforms every element in an array to another value
+
+  function map(array) {
+  for (const element of array) {
+    // Do something to each element
+  }
+}
+
+
+Callback:
+We want map to only be concerned with iterating over the collection and passing each element to a callback
+
+Example:
+
+function map(array, callback) {
+  for (const element of array) {
+    callback(element);
+  }
+}
+
+
+map([1, 2, 3], function (num) {
+  console.log(num * num);
+});
+// LOG: 1
+// LOG: 4
+// LOG: 9
+
+
+So we want to map and array, then do something with that data. map pulls it apart, and the call back function does something with those values to return a new value
+
+
+
+const oldAccounts = [
+  { userID: 15, title: "Developer Apprentice", accessLevel: "user" },
+  { userID: 63, title: "Developer Apprentice", accessLevel: "user" },
+  { userID: 97, title: "Developer Apprentice", accessLevel: "user" },
+  { userID: 12, title: "Developer Apprentice", accessLevel: "user" },
+  { userID: 44, title: "Developer Apprentice", accessLevel: "user" },
+];
+
+const newEngineers = map(oldAccounts, function (account) {
+  return Object.assign({}, account, { accessLevel: "admin" });
+});
+
+- given oldAccounts, we want to create a new object for the newEngineers
+- newEngineers is made up of the oldAccounts, but we are changing the accessLevel from "user" to "admin"
+
+we want to map(oldAccount, callback(each account)){
+  and return the object, but after assigning(each account, with new accessLevel, of "admin")
+}
+
+  */
